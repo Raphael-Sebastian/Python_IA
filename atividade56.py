e1a8dd080b653afbeee1cf5d86fd8f2e13af0914
Rafael Sebastian <rafaelsebastiansilva2003@gmail.com>
2025-05-15 15:32:40 -0300

    atividade 56

---
+++ atividade56.py
@@ -0,0 +1,22 @@
+
+
+
+# while True:
+#     escreva = input("Escreva uma String: ")
+#     if escreva: 
+#         print(f"("_"){escreva}")
+#     break
+
+while True:
+    
+    
+    texto = (input("Digite uma string: "))
+    if texto != "":
+        
+     print(texto)
+     print("-"*len(texto))
+     
+    else:
+        break
+
+    
