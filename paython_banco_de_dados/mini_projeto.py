e280dba84dc51645ac5c49febaf2b8dc5776f590
Rafael <rafaelsebastiansilva2003@gmail.com>
2025-08-29 16:52:28 -0300

    29/8/25

--- paython_banco_de_dados/mini_projeto.py
+++ paython_banco_de_dados/mini_projeto.py
@@ -6,3 +6,16 @@ engine = create_engine("mysql+pymysql://root:123456@localhost/sistema_vendas")
 
 df = pd.read_sql("SELECT preco_custo, preco_venda, estoque_atual from produtos", con=engine)
 
+preco_custo = df["preco_custo"].to_numpy()
+preco_venda = df["preco_venda"].to_numpy()
+estoque = df["estoque_atual"].to_numpy()
+lucro_unitario = preco_venda - preco_custo
+
+indice_rentabilidade = (lucro_unitario / preco_custo) * 100
+df["rentabilidade"] = np.round(indice_rentabilidade,2)
+
+rentabilidade_media = df["rentabilidade"].mean()
+
+abaixo_da_media = df[df["rentabilidade"] < rentabilidade_media]
+
+print(abaixo_da_media)
---
+++ paython_banco_de_dados/modulo_4.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sqlalchemy import create_engine
+
+engine = create_engine("mysql+pymysql://root:123456@localhost/sistema_vendas")
+
+query = """SELECT 
+    p.nome_produto,
+    SUM(i.quantidade) AS total_vendido
+FROM itens_pedido i
+JOIN produtos p ON i.produto_id = p.produto_id
+GROUP BY p.nome_produto
+ORDER BY total_vendido DESC
+LIMIT 10 """
+
+df = pd.read_sql(query,con=engine)
+
+#MatPlotlib
+plt.figure(figsize=(10,6))
+plt.bar(df["nome_produto"], df["total_vendido"], color="skyblue")
+plt.title("Top 10 produtos Mais vendidos")
+plt.xlabel("Produto")
+plt.ylabel("Quantidade Vendida")
+plt.xticks(rotation=45, ha="right")
+plt.show()
+
+#Gráfico de pizza
+plt.figure(figsize=(8,8))
+plt.pie(df["total_vendido"], labels=df["nome_produto"], autopct="%1.1f%%")
+plt.title("Distribuição de vendas dos TOP 10 Produtos")
+plt.show()
+
+query2= """SELECT
+    data_pedido AS mes,
+    COUNT(*) AS total_pedidos
+FROM pedidos
+GROUP BY mes
+ORDER BY mes
+"""
+df_meses = pd.read_sql(query2,con=engine)
+plt.figure(figsize=(10,6))
+plt.plot(df_meses["mes"], df_meses["total_pedidos"], marker="o", linestyle="-")
+plt.title("Evolução de pedidos por Mês")
+plt.xlabel("Mês")
+plt.ylabel("Total de Pedidos")
+plt.xticks(rotation=45)
+plt.grid(True)
+plt.tight_layout()
+plt.show()
